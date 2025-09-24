5b200ecc32f0013968a8f297254d9d7b39745292
gyanesh4u <37295102+gyanesh4u@users.noreply.github.com>
2025-09-24 10:09:26 +0530

    checkbox

--- src/com/qa/test/Demo8.java
+++ src/com/qa/test/Demo8.java
@@ -8,13 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo8 {
 
 	public static void main(String[] args) {
-		
-		WebDriver driver=new ChromeDriver();
+
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
 		WebElement gmail = driver.findElement(By.xpath("//a[.='Gmail']"));
 		boolean flag = gmail.isDisplayed();
 		System.out.println(flag);
-		WebElement search = driver.findElement(By.xpath("//input[@data-ved='0ahUKEwiwp8X13KKPAxV4xzgGHX01LZAQ4dUDCCA']"));
+		WebElement search = driver
+				.findElement(By.xpath("//input[@data-ved='0ahUKEwiwp8X13KKPAxV4xzgGHX01LZAQ4dUDCCA']"));
 		System.out.println(search.isEnabled());
 	}
 }
